c8364b75139cfe2894e4d92e00495ee9abb52741
minhquan2812 <ho.van.minh.quan.1997@gmail.com>
2019-06-04 17:06:48 +0700

    Add prototype eat

--- cat.js
+++ cat.js
@@ -1,6 +1,11 @@
 function cat(name, color) {
 	this.name = name;
 	this.color = color;
+	this.stomach = [];
 };
+
+cat.prototype.eat = function (mouse) {
+	this.stomach.push(mouse);
+}
 
 module.exports = cat;
--- mouse.js
+++ mouse.js
@@ -1,6 +1,7 @@
 function mouse(name, color) {
 	this.name = name; 
 	this.color  = color;
+	this.stomach = [];
 };
 
 module.exports = mouse;
